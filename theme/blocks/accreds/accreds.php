6426753edd7ee77e343427f804eb121c6552c05b
lliwio <thomasdean23@gmail.com>
2024-10-29 09:52:50 +0000

    Amends

--- theme/blocks/accreds/accreds.php
+++ theme/blocks/accreds/accreds.php
@@ -1,7 +1,15 @@
 <?php if (have_rows('logos')) : ?>
-    <div class="max-w-wide w-full mx-auto px-4 2xl:px-0 relative mb-12">
-        <div class="swiper carousel-swiper">
-            <div class="swiper-wrapper">
+<section class="py-6">
+    <div class="max-w-wide mx-auto px-4 2xl:px-0">
+        <?php if(the_field('accreds_title')): ?>
+        <h3 class="text-2xl uppercase table bg-foreground text-yellow px-4 py-2 mx-auto">
+                <?php the_field('accreds_title'); ?>
+        </h3>
+        <?php endif; ?>
+    </div>
+    <div class="alignfull mx-auto px-4 2xl:px-0 relative mb-12">
+        <div class="logos group relative overflow-hidden whitespace-nowrap my-4 [mask-image:_linear-gradient(to_right,_transparent_0,_white_128px,white_calc(100%-128px),_transparent_100%)]">
+            <div class="animate-slide-left infinite group-hover:animation-pause inline-block w-max">
                 <?php
                 // Initialize a counter for numbering the slides
                 $counter = 1;
@@ -10,9 +18,9 @@
                 while (have_rows('logos')) : the_row();
                     $image = get_sub_field('image');
                 ?>
-                <div class="swiper-slide bg-white pb-4 px-8">
+                <div class="inline w-64 px-2">
                     <?php if ($image) : ?>
-                        <img class="h-24 object-contain" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                        <img loading="lazy" class="mx-2 inline w-44" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
                     <?php else : ?>
                        
                     <?php endif; ?>
@@ -24,9 +32,8 @@
                 ?>
             </div>
         </div>
-        <div class="swiper-button-next static !top-24 !right-0 xl:!-right-8 !w-6 !h-6 p-4"></div>
-        <div class="swiper-button-prev static !top-24 !left-0 xl:!-left-8 !w-6 !h-6 p-4"></div>
     </div>
+</section>
 <?php else : ?>
     <p>No rows found in repeater.</p>
 <?php endif; ?>
