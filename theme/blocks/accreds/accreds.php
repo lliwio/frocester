01386d4a7745aba5f299854ea4f5e20ec0611ef9
lliwio <thomasdean23@gmail.com>
2024-10-31 12:10:12 +0000

    more

--- theme/blocks/accreds/accreds.php
+++ theme/blocks/accreds/accreds.php
@@ -1,11 +1,15 @@
 <?php if (have_rows('logos')) : ?>
 <section class="py-6">
     <div class="max-w-wide mx-auto px-4 2xl:px-0">
-        <?php if(the_field('accreds_title')): ?>
-        <h3 class="text-2xl uppercase table bg-foreground text-yellow px-4 py-2 mx-auto">
-                <?php the_field('accreds_title'); ?>
-        </h3>
-        <?php endif; ?>
+    <?php 
+$title = get_field('accreds_title'); // Get the field value
+if ($title): // Check if the field has a value
+?>
+    <h3 class="text-2xl uppercase table bg-foreground text-yellow px-4 py-2 mx-auto">
+        <?php echo esc_html($title); // Output the title with escaping ?>
+    </h3>
+<?php endif; ?>
+
     </div>
     <div class="alignfull mx-auto px-4 2xl:px-0 relative mb-12">
         <div class="logos group relative overflow-hidden whitespace-nowrap my-4 [mask-image:_linear-gradient(to_right,_transparent_0,_white_128px,white_calc(100%-128px),_transparent_100%)]">
